f27db45d911bcd30b46a4ef11f3054ae1c418919
Shakhnoza Tokhtakhunova <chaknka@gmail.com>
2022-11-16 19:45:34 -0500

    Updated after stash

--- src/ProjectTestGit/HelloWorld.java
+++ src/ProjectTestGit/HelloWorld.java
@@ -2,4 +2,8 @@ package ProjectTestGit;
 
 public class HelloWorld {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Hi!");
+	}
 }
---
+++ src/ProjectTestGit2/ClassicWorld.java
@@ -0,0 +1,11 @@
+package ProjectTestGit2;
+
+public class ClassicWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Done!");
+
+	}
+
+}
